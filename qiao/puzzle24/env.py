9d4f64fdc18e9817aa47e12cee2d11c6da17ffbc
goldolphin <goldolphin@gmail.com>
2024-02-07 13:46:38 +0800

    tmp

--- qiao/puzzle24/env.py
+++ qiao/puzzle24/env.py
@@ -1,6 +1,42 @@
 import gymnasium
+from gymnasium import spaces
+
 
 class Puzzle24Env(gymnasium.Env):
-    def __init__(self) -> None:
-        super().__init__()
-        
+    def __init__(self, max_seq_len: int) -> None:
+        self.observation_space = spaces.Discrete(max_seq_len + 1)   # candiates + expected results
+        spaces.Discrete
+        self.observation_space = spaces.Dict(
+            {
+                "agent": spaces.Box(0, size - 1, shape=(2,), dtype=int),
+                "target": spaces.Box(0, size - 1, shape=(2,), dtype=int),
+            }
+        )
+
+        # We have 4 actions, corresponding to "right", "up", "left", "down"
+        self.action_space = spaces.Discrete(4)
+
+        """
+        The following dictionary maps abstract actions from `self.action_space` to
+        the direction we will walk in if that action is taken.
+        I.e. 0 corresponds to "right", 1 to "up" etc.
+        """
+        self._action_to_direction = {
+            0: np.array([1, 0]),
+            1: np.array([0, 1]),
+            2: np.array([-1, 0]),
+            3: np.array([0, -1]),
+        }
+
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
+        self.render_mode = render_mode
+
+        """
+        If human-rendering is used, `self.window` will be a reference
+        to the window that we draw to. `self.clock` will be a clock that is used
+        to ensure that the environment is rendered at the correct framerate in
+        human-mode. They will remain `None` until human-mode is used for the
+        first time.
+        """
+        self.window = None
+        self.clock = None
